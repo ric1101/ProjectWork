7f92411e01d366e6a33186eeaf9c8510b624f6d2
ric1101 <161829770+ric1101@users.noreply.github.com>
2024-05-23 15:31:50 +0200

    push aggiornamento img

--- ProjectWork/js/disponibilita.js
+++ ProjectWork/js/disponibilita.js
@@ -14,15 +14,17 @@ function popolaDisponibilita() {
   fetch(URLT)
     .then(res => res.json())
     .then(data => {
-      stampa(data);
-
+      stampa(data.trucks, data.marchio.nome, data.marchio.genere);
+      console.log(data);
     })
 
 }
 popolaDisponibilita();
 
 
-function stampa(array) {
+function stampa(array, marchio, genere) {
+  let marchio1 = marchio;
+  let genere1 = genere;
   array.forEach(element => {
     if (element.disponibilità == true) {
       all.innerHTML += `<div class="card m-4 col-lg-3 col-md-4 col-sm-6 mb-4">
@@ -41,16 +43,17 @@ function stampa(array) {
     <h5 class="card-title text-center">${element.nome}</h5>
   </a>
   <div class="indirizzo ms-auto col-8">${element.indirizzo}</div>
-  <div class=" categoria  ms-auto col-4">categoria</div>
+  <div class=" categoria  ms-auto col-4">${marchio1}</div>
+  <div class=" categoria  ms-auto col-4">${genere1}</div>
   </div>
   </div>`;
 
     } else {
       all.innerHTML += `<div class="card m-4 col-lg-3 col-md-4 col-sm-6 mb-4">
   <div class="card-body  p-0 ">
-  <div class="box-image">
+  <div class="box-image" style="height: 100%;">
 
-    <img src="${element.immagine}" class="card-img-top scurita" alt="">
+    <img src="${element.immagine}" class="card-img-top scurita" alt="" style="height: 100%;">
   </div>
   </div>
   <ul class="list-group ">
@@ -62,7 +65,8 @@ function stampa(array) {
     <h5 class="card-title text-center col-12">${element.nome}</h5>
 
   <div class="indirizzo ms-auto col-8">${element.indirizzo}</div>
-  <div class=" categoria  ms-auto col-4">categoria</div>
+  <div class=" categoria  ms-auto col-4">${marchio1}</div>
+  <div class=" categoria  ms-auto col-4">${genere1}</div>
   </div>
   </div>`;
 
--- ProjectWork/js/trucks.js
+++ ProjectWork/js/trucks.js
@@ -1,1 +1,72 @@
 const URLT = 'http://localhost:8080/api/foodtrucks';
+
+let all = document.querySelector('.all');
+let marchio = document.querySelector('.marchio');
+const URLMARCHI = 'http://localhost:8080/api/marchi';
+
+
+function popolaDisponibilita() {
+
+  fetch(URLT)
+    .then(res => res.json())
+    .then(data => {
+      stampa(data /*, data.marchio.nome, data.marchio.genere*/);
+      console.log(data);
+    })
+
+}
+popolaDisponibilita();
+
+
+function stampa(array/*, marchio, genere*/) {
+//   let marchio1 = marchio;
+//   let genere1 = genere;
+  array.forEach(element => {
+    if (element.disponibilità == true) {
+      all.innerHTML += `<div class="card m-4 col-lg-3 col-md-4 col-sm-6 mb-4">
+  <div class="card-body  p-0 ">
+  <div class="box-image" style="height: 100%;">
+          
+    <img src="${element.immagine}" class="card-img-top" alt="" style="height: 100%;">
+  </div>
+  </div>
+  <ul class="list-group ">
+  <li class="list-group-item disponibile">Disponibile <i class="fas fa-check-circle text-success"></i>
+  </li>
+  </ul>
+  <div class="card-body row">
+  <a href="truck.html" class="card-title-link col-12">
+    <h5 class="card-title text-center">${element.nome}</h5>
+  </a>
+  <div class="indirizzo ms-auto col-8">${element.indirizzo}</div>
+  <div class=" categoria  ms-auto col-4"></div>
+  <div class=" categoria  ms-auto col-4"></div>
+  </div>
+  </div>`;
+
+    } else {
+      all.innerHTML += `<div class="card m-4 col-lg-3 col-md-4 col-sm-6 mb-4">
+  <div class="card-body  p-0 ">
+  <div class="box-image" style="height: 100%;">
+
+    <img src="${element.immagine}" class="card-img-top scurita" alt="" style="height: 100%;">
+  </div>
+  </div>
+  <ul class="list-group ">
+  <li class="list-group-item disponibile">Non disponibile <i class="fas fa-times-circle text-danger"></i>
+  </li>
+  </ul>
+  <div class="card-body row ">
+
+    <h5 class="card-title text-center col-12">${element.nome}</h5>
+
+  <div class="indirizzo ms-auto col-8">${element.indirizzo}</div>
+  <div class=" categoria  ms-auto col-4"></div>
+  <div class=" categoria  ms-auto col-4"></div>
+  </div>
+  </div>`;
+
+    }
+
+  });
+}
