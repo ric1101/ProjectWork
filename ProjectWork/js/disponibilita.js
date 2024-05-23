7272cc2f2675fbd4d47d79eff3b82fd87ee4934e
Mirko Di Franco <120678275+mirko-di-franco@users.noreply.github.com>
2024-05-23 12:45:51 +0200

    [add] function stampa

--- ProjectWork/js/disponibilita.js
+++ ProjectWork/js/disponibilita.js
@@ -4,64 +4,69 @@ const URLMARCHI = 'http://localhost:8080/api/marchi';
 
 
 function popolaDisponibilita() {
-    let id = JSON.parse(localStorage.getItem('id'));
-    let nome = JSON.parse(localStorage.getItem('nome'));
-    marchio.innerHTML = nome;
-    console.log(nome);
-    console.log(id);
-    const URLT = `http://localhost:8080/api/foodtrucks/marchio/id/${id}`;
+  let id = JSON.parse(localStorage.getItem('id'));
+  let nome = JSON.parse(localStorage.getItem('nome'));
+  marchio.innerHTML = nome;
+  console.log(nome);
+  console.log(id);
+  const URLT = `http://localhost:8080/api/foodtrucks/marchio/id/${id}`;
 
-    fetch(URLT)
-        .then(res => res.json())
-        .then(data => {
-            data.forEach(element => {
-                if (element.disponibilità == true) {
-                    all.innerHTML += `<div class="card m-4 col-lg-3 col-md-4 col-sm-6 mb-4">
-          <div class="card-body  p-0 ">
-            <div class="box-image" style="height: 100%;">
-                    
-              <img src="${element.immagine}" class="card-img-top" alt="" style="height: 100%;">
-            </div>
-          </div>
-          <ul class="list-group ">
-            <li class="list-group-item disponibile">Disponibile <i class="fas fa-check-circle text-success"></i>
-            </li>
-          </ul>
-          <div class="card-body row">
-            <a href="truck.html" class="card-title-link col-12">
-              <h5 class="card-title text-center">${element.nome}</h5>
-            </a>
-            <div class="indirizzo ms-auto col-8">${element.indirizzo}</div>
-            <div class=" categoria  ms-auto col-4">categoria</div>
-          </div>
-        </div>`;
+  fetch(URLT)
+    .then(res => res.json())
+    .then(data => {
+      stampa(data);
 
-                } else {
-                    all.innerHTML += `<div class="card m-4 col-lg-3 col-md-4 col-sm-6 mb-4">
-          <div class="card-body  p-0 ">
-            <div class="box-image">
-
-              <img src="${element.immagine}" class="card-img-top scurita" alt="">
-            </div>
-          </div>
-          <ul class="list-group ">
-            <li class="list-group-item disponibile">Non disponibile <i class="fas fa-times-circle text-danger"></i>
-            </li>
-          </ul>
-          <div class="card-body row ">
-           
-              <h5 class="card-title text-center col-12">${element.nome}</h5>
-           
-            <div class="indirizzo ms-auto col-8">${element.indirizzo}</div>
-            <div class=" categoria  ms-auto col-4">categoria</div>
-          </div>
-        </div>`;
-
-                }
-
-            });
-
-        })
+    })
 
 }
 popolaDisponibilita();
+
+
+function stampa(array) {
+  array.forEach(element => {
+    if (element.disponibilità == true) {
+      all.innerHTML += `<div class="card m-4 col-lg-3 col-md-4 col-sm-6 mb-4">
+  <div class="card-body  p-0 ">
+  <div class="box-image" style="height: 100%;">
+          
+    <img src="${element.immagine}" class="card-img-top" alt="" style="height: 100%;">
+  </div>
+  </div>
+  <ul class="list-group ">
+  <li class="list-group-item disponibile">Disponibile <i class="fas fa-check-circle text-success"></i>
+  </li>
+  </ul>
+  <div class="card-body row">
+  <a href="truck.html" class="card-title-link col-12">
+    <h5 class="card-title text-center">${element.nome}</h5>
+  </a>
+  <div class="indirizzo ms-auto col-8">${element.indirizzo}</div>
+  <div class=" categoria  ms-auto col-4">categoria</div>
+  </div>
+  </div>`;
+
+    } else {
+      all.innerHTML += `<div class="card m-4 col-lg-3 col-md-4 col-sm-6 mb-4">
+  <div class="card-body  p-0 ">
+  <div class="box-image">
+
+    <img src="${element.immagine}" class="card-img-top scurita" alt="">
+  </div>
+  </div>
+  <ul class="list-group ">
+  <li class="list-group-item disponibile">Non disponibile <i class="fas fa-times-circle text-danger"></i>
+  </li>
+  </ul>
+  <div class="card-body row ">
+
+    <h5 class="card-title text-center col-12">${element.nome}</h5>
+
+  <div class="indirizzo ms-auto col-8">${element.indirizzo}</div>
+  <div class=" categoria  ms-auto col-4">categoria</div>
+  </div>
+  </div>`;
+
+    }
+
+  });
+}
