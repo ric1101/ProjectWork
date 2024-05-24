cbe34fce6e74d8db3dd49380562f0ba4c2b64c37
ric1101 <161829770+ric1101@users.noreply.github.com>
2024-05-24 10:59:11 +0200

    sistemata pagina truck

--- ProjectWork/js/truck.js
+++ ProjectWork/js/truck.js
@@ -65,3 +65,38 @@ let nuovoPiatto = `<div class="col-md-3 mt-4">
     <a href="#" class="card-button"> Acquista</a>
 </div>
 </div>`;
+
+let videoIndirizzo = `<div class="container-fluid px-0">
+<div class="row gx-0">
+    <div class="col-lg-6">
+        <div class="ratio ratio-16x9">
+            <video autoplay loop muted>
+                <source src="./video/${vid}.mp4" type="video/mp4">
+                <source src="./video/${vid}.mp4" type="video/ogg">
+            </video>
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div class="card rounded-0 info-truck h-100">
+            <div class="card-body d-flex">
+                <div class="col">
+                    <h3>Indirizzo</h3>
+                    <p>${via}</p>
+                    <h3>Categoria</h3>
+                    <p>${cat}</p>
+                    <strong>
+                        <li class="list-group-item disponibile">Disponibile <i
+                                class="fas fa-check-circle text-success"></i>
+                    </strong>
+                </div>
+                <div class="col">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19924.623222181126!2d12.559323604942811!3d42.06862829715841!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f6fe57d615101%3A0x5fc0b897bcc5725e!2sRoma%20City%20FC!5e0!3m2!1sit!2sit!4v1716209872872!5m2!1sit!2sit"
+                        width="800" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`
