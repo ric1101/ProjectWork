57649c8ac0d56379961f506827cfec1071e5478a
ric1101 <161829770+ric1101@users.noreply.github.com>
2024-05-26 15:13:54 +0200

    img logos

--- ProjectWork/js/login.js
+++ ProjectWork/js/login.js
@@ -1,48 +1,9 @@
+const URLOG = `http://localhost:8080/api/utenti/login`;
 
-let listaErrori = document.querySelector('#listaErrori');
+let errore = document.querySelector('.errore');
 let register = document.querySelector('#register');
-let username = document.querySelector('#username');
-let password = document.querySelector('#password');
-let button = document.querySelector('.btn');
-let usernameError = document.querySelector('#usernameError');
 let showPassword = document.querySelector('#showPassword');
-let registerBtn = document.querySelector('#registerBtn');
-
-const regexUSERNAME = /^[a-zA-Z0-9]{4,15}$/;
-const regexPASSWORD = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-
-
-function usernameCheck() {
-    if (username.value.match(regexUSERNAME)) {
-        usernameError.textContent = ''
-        return true;
-    } else if (username.value.length < 4) {
-        usernameError.textContent = "L'username è inferiore a 4 caratteri";
-    } else if (username.value.length > 15) {
-        usernameError.textContent = "L'username è superiore a 15 caratteri";
-    } else {
-        usernameError.textContent = "Non puoi inserire uno spazio";
-    }
-}
-
-function passwordCheck() {
-    if (password.value.match(regexPASSWORD)) {
-        listaErrori.textContent = ''
-        return true;
-    } else {
-        listaErrori.innerHTML = `<li>La tua password deve contenere almeno:</li>
-                                 <li>Almeno un carattere in maiuscolo</li>
-                                 <li>Almeno un carattere in minuscolo</li>
-                                 <li>Almeno un numero</li>
-                                 <li>Almeno un carattere speciale (!,?,@)</li>
-                                 `
-    }
-}
-
-username.addEventListener('keyup', usernameCheck);
-
-password.addEventListener('keyup', passwordCheck);
+let button = document.querySelector('.btn');
 
 function toShowPassword() {
     if (password.type === 'text') {
@@ -52,3 +13,43 @@ function toShowPassword() {
     }
 }
 showPassword.addEventListener('click', toShowPassword);
+
+
+class Utente {
+    constructor(email, password) {
+        (this.email = email),
+        (this.password = password)
+    }
+  }
+  
+  let nuovoUtente = {};
+  
+  console.log(JSON.stringify(nuovoUtente));
+  
+  function login() {
+    nuovoUtente = new Utente(
+      email.value,
+      password.value
+    );
+    console.log(nuovoUtente);
+  
+    fetch(URLOG, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(nuovoUtente),
+      
+    })
+    .then(response =>{
+        
+        if (response.ok) {
+            window.location.href = 'index.html';
+            localStorage.setItem('');
+        } else {
+            errore.innerHTML = 'email o password errati';
+        }
+      console.log(nuovoUtente);
+    });
+  }
+  button.addEventListener('click', login);
